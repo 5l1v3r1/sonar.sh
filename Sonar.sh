3e9010d59ead80cdbaee9a0330f1819228519e85
Grimmz <57014148+thatonetester@users.noreply.github.com>
2019-12-24 18:42:21 -0500

    Create Sonar.sh

---
+++ Sonar.sh
@@ -0,0 +1,9 @@
+sonar () { 
+url=$1
+
+curl https://tls.bufferover.run/dns?q=$1 | jq >> sonar_output.txt
+
+cat sonar_output.txt | jq '.Results' | gron | awk -v RS='([0-9]+\\.){3}[0-9]+' 'RT{print RT}'| tee -a sonar_IPs_$url.txt
+}
+
+sonar $1
