75ff7cf90429e3c5ab3a1769aeffdf5a8572c723
Grimmz <57014148+thatonetester@users.noreply.github.com>
2020-01-16 16:08:02 -0500

    Update Sonar.sh

--- Sonar.sh
+++ Sonar.sh
@@ -1,7 +1,7 @@
 sonar () { 
 url=$1
 
-curl https://tls.bufferover.run/dns?q=$1 | jq >> sonar_output.txt
+curl https://tls.bufferover.run/dns?q=$url | jq >> sonar_output.txt
 
 cat sonar_output.txt | jq '.Results' | gron | awk -v RS='([0-9]+\\.){3}[0-9]+' 'RT{print RT}'| tee -a sonar_IPs_$url.txt
 }
